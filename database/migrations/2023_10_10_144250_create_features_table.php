eba9c875d83450851b74ab9a0900d8ff1376052d
Ali Dakkak <aliangel140@gmail.com>
2023-10-11 13:22:10 +0300

    s

--- database/migrations/2023_10_10_144250_create_features_table.php
+++ database/migrations/2023_10_10_144250_create_features_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->string('image')->nullable();
             $table->foreignId('category_id')->references('id')->on('categories')
                 ->onDelete('cascade');
